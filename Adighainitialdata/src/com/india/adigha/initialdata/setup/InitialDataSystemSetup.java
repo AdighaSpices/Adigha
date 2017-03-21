fd0be0476ff07ea77af7f072d2c7f5c50401b17b
santosh bomma <santosh.bomma@boeing.com>
2017-03-21 20:03:05 +0530

    Configured SystemSetup class for InitialData project

--- Adighainitialdata/src/com/india/adigha/initialdata/setup/InitialDataSystemSetup.java
+++ Adighainitialdata/src/com/india/adigha/initialdata/setup/InitialDataSystemSetup.java
@@ -14,24 +14,28 @@ package com.india.adigha.initialdata.setup;
 import de.hybris.platform.commerceservices.dataimport.impl.CoreDataImportService;
 import de.hybris.platform.commerceservices.dataimport.impl.SampleDataImportService;
 import de.hybris.platform.commerceservices.setup.AbstractSystemSetup;
+import de.hybris.platform.commerceservices.setup.data.ImportData;
+import de.hybris.platform.commerceservices.setup.events.CoreDataImportedEvent;
 import de.hybris.platform.core.initialization.SystemSetup;
 import de.hybris.platform.core.initialization.SystemSetup.Process;
 import de.hybris.platform.core.initialization.SystemSetup.Type;
 import de.hybris.platform.core.initialization.SystemSetupContext;
 import de.hybris.platform.core.initialization.SystemSetupParameter;
 import de.hybris.platform.core.initialization.SystemSetupParameterMethod;
-import com.india.adigha.initialdata.constants.AdighaInitialDataConstants;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Required;
 
+import com.india.adigha.initialdata.constants.AdighaInitialDataConstants;
+
 
 /**
  * This class provides hooks into the system's initialization and update processes.
- * 
+ *
  * @see "https://wiki.hybris.com/display/release4/Hooks+for+Initialization+and+Update+Process"
  */
 @SystemSetup(extension = AdighaInitialDataConstants.EXTENSIONNAME)
@@ -42,10 +46,15 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 
 	private static final String IMPORT_CORE_DATA = "importCoreData";
 	private static final String IMPORT_SAMPLE_DATA = "importSampleData";
-	private static final String ACTIVATE_SOLR_CRON_JOBS = "activateSolrCronJobs";
+	//private static final String ACTIVATE_SOLR_CRON_JOBS = "activateSolrCronJobs";
+
+	private static final String IMPORT_SYNC_CATALOGS = "syncProducts&ContentCatalogs";
+	private static final String ADIGHA = "adighas";
 
 	private CoreDataImportService coreDataImportService;
 	private SampleDataImportService sampleDataImportService;
+
+
 
 	/**
 	 * Generates the Dropdown and Multi-select boxes for the project data import
@@ -57,8 +66,10 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 		final List<SystemSetupParameter> params = new ArrayList<SystemSetupParameter>();
 
 		params.add(createBooleanSystemSetupParameter(IMPORT_CORE_DATA, "Import Core Data", true));
+		params.add(createBooleanSystemSetupParameter(IMPORT_SYNC_CATALOGS, "Sync Products & Content Catalogs", true));
+
 		params.add(createBooleanSystemSetupParameter(IMPORT_SAMPLE_DATA, "Import Sample Data", true));
-		params.add(createBooleanSystemSetupParameter(ACTIVATE_SOLR_CRON_JOBS, "Activate Solr Cron Jobs", true));
+		//params.add(createBooleanSystemSetupParameter(ACTIVATE_SOLR_CRON_JOBS, "Activate Solr Cron Jobs", true));
 		// Add more Parameters here as you require
 
 		return params;
@@ -67,7 +78,7 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 	/**
 	 * Implement this method to create initial objects. This method will be called by system creator during
 	 * initialization and system update. Be sure that this method can be called repeatedly.
-	 * 
+	 *
 	 * @param context
 	 *           the context provides the selected parameters and values
 	 */
@@ -104,10 +115,71 @@ public class InitialDataSystemSetup extends AbstractSystemSetup
 	@SystemSetup(type = Type.PROJECT, process = Process.ALL)
 	public void createProjectData(final SystemSetupContext context)
 	{
+		final boolean syncCatalogs = getBooleanSystemSetupParameter(context, IMPORT_SYNC_CATALOGS);
+
 		/*
 		 * Add import data for each site you have configured
 		 */
+		final List<ImportData> importData = new ArrayList<ImportData>();
+
+		final ImportData adighasImportData = new ImportData();
+		adighasImportData.setProductCatalogName(ADIGHA);
+		adighasImportData.setContentCatalogNames(Arrays.asList(ADIGHA));
+		adighasImportData.setStoreNames(Arrays.asList(ADIGHA));
+		importData.add(adighasImportData);
+
+		if (getBooleanSystemSetupParameter(context, IMPORT_CORE_DATA))
+		{
+			getCoreDataImportService().execute(this, context, importData);
+			getEventService().publishEvent(new CoreDataImportedEvent(context, importData));
+
+			super.createContentCatalogSyncJob(context, "adighasContentCatalog");
+			importImpexFile(context, "/Adighainitialdata/import/coredata/stores/adighas/cart-removal-trigger.impex");
+		}
+
+		if (getBooleanSystemSetupParameter(context, IMPORT_SAMPLE_DATA))
+		{
+			getSampleDataImportService().execute(this, context, importData);
+		}
+
+		//		if (getBooleanSystemSetupParameter(context, IMPORT_SAMPLE_CUSTOMER))
+		//		{
+		//			importCustomerArchive(context);
+		//		}
+
+		//		if (getBooleanSystemSetupParameter(context, IMPORT_SAMPLE_PRODUCT))
+		//		{
+		//			importProductArchive(context);
+		//		}
+
+		if (syncCatalogs)
+		{
+			syncProductCatalog(context, adighasImportData.getProductCatalogName());
+
+			for (final String contentCatalogName : adighasImportData.getContentCatalogNames())
+			{
+				syncContentCatalog(context, contentCatalogName);
+			}
+		}
 	}
+
+	/**
+	 * fires product catalog with synchronization with respect for a dependent CatalogVersionSyncJob
+	 */
+	private void syncProductCatalog(final SystemSetupContext context, final String productCatalogName)
+	{
+		executeCatalogSyncJob(context, productCatalogName + "ProductCatalog");
+	}
+
+	/**
+	 * fires content catalog with synchronization with respect for a depends on CatalogVersionSyncJob
+	 *
+	 */
+	private void syncContentCatalog(final SystemSetupContext context, final String contentCatalogName)
+	{
+		executeCatalogSyncJob(context, contentCatalogName + "ContentCatalog");
+	}
+
 
 	public CoreDataImportService getCoreDataImportService()
 	{
